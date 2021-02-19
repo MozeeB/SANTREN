44e56f8eed527feed63e2ccac56b845773282029
mujibbur741 <mujiburr741@gmail.com>
2021-02-19 14:02:57 +0700

    starting santren

---
+++ app/src/main/java/com/cikup/santren/presentation/navigation/Navigation.kt
@@ -0,0 +1,22 @@
+package com.cikup.santren.presentation.navigation
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+
+//fun navigateToRegister(context: Context) {
+//    if (context != null && context is Activity) {
+//        val activity = context
+//        val flags = context.flags(Intent.FLAG_ACTIVITY_NEW_TASK, Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        activity.start<RegistrationActivity>(flags, right_in, left_out)
+//    }
+//}
+
+//fun backToDetailProduct(context: Context, bundle: Bundle) {
+//    if (context != null && context is Activity) {
+//        val activity = context
+//        val flags = context.flags(Intent.FLAG_ACTIVITY_NEW_TASK, Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        activity.start<DetailProdukActivity>(bundle, flags, left_in, right_out)
+//    }
+//}
